365a8dae527b4a81c607150d47b90effd29520a5
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-02-24 14:56:01 +0100

    path...

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -13,13 +13,12 @@ mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 cd out
 echo "Show files from folder out"
 ls
-cd ..
 echo "Show Arduino directory"
-ls components/arduino
+ls ../components/arduino
 echo "Creating framework-arduinoespressif32"
 #git clone https://github.com/tasmota/arduino-esp32
 #git clone https://github.com/tasmota/arduino-esp32 -b esp32-s3-support arduino-esp32
-cp -rf components/arduino arduino-esp32
+cp -rf ../components/arduino arduino-esp32
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/sdk
