b0e60efecea858c63c4f9675399046e05f08a78d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-07-29 14:48:08 +0200

    Revert "2041 idf443 (#28)"
    This reverts commit d66ef429da2fe7bcb0321666780ad9355ead0dfc.

--- tools/config.sh
+++ tools/config.sh
@@ -6,7 +6,7 @@ if [ -z $IDF_PATH ]; then
 fi
 
 if [ -z $IDF_BRANCH ]; then
-	IDF_BRANCH="442_esp41"
+	IDF_BRANCH="v4.4.2"
 fi
 
 if [ -z $AR_PR_TARGET_BRANCH ]; then
@@ -34,7 +34,7 @@ AR_USER="tasmota"
 AR_REPO="$AR_USER/arduino-esp32"
 
 # Arduino branch to use
-AR_BRANCH="speed_OTA"
+AR_BRANCH="work"
 
 AR_REPO_URL="https://github.com/$AR_REPO.git"
 if [ -n $GITHUB_TOKEN ]; then
