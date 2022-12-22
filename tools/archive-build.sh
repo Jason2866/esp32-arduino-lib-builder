c5e78cd02ca32ae76c4259e365f66308ab301d75
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-12-22 23:34:52 +0100

    Itead core 2.0.6 (#24)

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD || echo "")
-IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || echo "")
+IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || git -C "$IDF_PATH" tag --points-at HEAD || echo "")
 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 archive_path="dist/arduino-esp32-libs-ITEAD-$idf_version_string.tar.gz"
@@ -16,16 +16,19 @@ echo "Creating framework-arduinoespressif32-ITEAD"
 cp -rf ../components/arduino arduino-esp32
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/tests
+rm -rf arduino-esp32/libraries/RainMaker
+rm -rf arduino-esp32/libraries/Insights
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/sdk
 rm -rf arduino-esp32/tools/esptool.py
 rm -rf arduino-esp32/tools/gen_esp32part.py
+rm -rf arduino-esp32/tools/gen_insights_package.py
+rm -rf arduino-esp32/tools/gen_insights_package.exe
 rm -rf arduino-esp32/tools/platformio-build-*.py
 rm -rf arduino-esp32/platform.txt
 rm -rf arduino-esp32/package.json
 cp -f platform.txt arduino-esp32/
 cp -Rf tools/sdk arduino-esp32/tools/
-cp -f tools/esptool.py arduino-esp32/tools/
 cp -f tools/gen_esp32part.py arduino-esp32/tools/
 cp -f tools/platformio-build-*.py arduino-esp32/tools/
 cp ../package.json arduino-esp32/package.json
