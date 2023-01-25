3826e8be2cb87ec129d7217e74fbe98b8d920a06
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-01-25 14:12:48 +0100

    Update prepare-ci.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache jq p7zip-full
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
+sudo apt-get install -y flex bison gperf cmake ninja-build jq p7zip-full
 pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
