8c76ad1550f3f5976b8ce9a32f29407457141e07
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-01-25 13:38:18 +0100

    Fix Syntax

--- tools/install-esp-idf.sh
+++ tools/install-esp-idf.sh
@@ -30,10 +30,10 @@ fi
 
 if [ ! -x $idf_was_installed ] || [ ! -x $commit_predefined ]; then
 	git -C $IDF_PATH submodule update --init --recursive
-	set(IDF_VERSION_MAJOR 5)
-        set(IDF_VERSION_MINOR 1)
-        set(IDF_VERSION_PATCH 0)
-        set(ENV{IDF_VERSION} "${IDF_VERSION_MAJOR}.${IDF_VERSION_MINOR}.${IDF_VERSION_PATCH}")
+	IDF_VERSION_MAJOR=5
+        IDF_VERSION_MINOR=1
+        IDF_VERSION_PATCH=0
+        export IDF_VERSION="$IDF_VERSION_MAJOR.$IDF_VERSION_MINOR.$IDF_VERSION_PATCH"
 	echo "IDF_VERSION $IDF_VERSION"
 	$IDF_PATH/install.sh
 fi
