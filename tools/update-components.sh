4572134ce8f5a203b8622825b67c2a1c0285cbac
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-08-20 13:05:09 +0200

    fix typo

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -21,7 +21,7 @@ if [ $? -ne 0 ]; then exit 1; fi
 #
 # Arduino needs cam_hal.h from esp32-camera in include folder
 #
-cp "AR_COMPS$/esp32-camera/driver/private_include/cam_hal.h" "AR_COMPS$/esp32-camera/driver/include/"
+cp "$AR_COMPS/esp32-camera/driver/private_include/cam_hal.h" "$AR_COMPS/esp32-camera/driver/include/"
 
 #
 # CLONE/UPDATE ESP-LITTLEFS
