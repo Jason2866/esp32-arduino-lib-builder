6cdd540ae9f0adcbb9d69636301a03a616adc61f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-11-30 17:43:39 +0100

    Update update-components.sh

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -3,7 +3,6 @@
 source ./tools/config.sh
 
 CAMERA_REPO_URL="https://github.com/espressif/esp32-camera.git"
-LITTLEFS_REPO_URL="https://github.com/joltwallet/esp_littlefs.git"
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 
@@ -25,22 +24,6 @@ if [ $? -ne 0 ]; then exit 1; fi
 # Arduino needs cam_hal.h from esp32-camera in include folder
 #
 cp "$AR_COMPS/esp32-camera/driver/private_include/cam_hal.h" "$AR_COMPS/esp32-camera/driver/include/"
-
-#
-# CLONE/UPDATE ESP-LITTLEFS v1.10.0 commit 032f9...
-#
-#echo "Updating ESP-LITTLEFS..."
-#if [ ! -d "$AR_COMPS/esp_littlefs" ]; then
-#	git clone $LITTLEFS_REPO_URL "$AR_COMPS/esp_littlefs"
-# 	git -C "$AR_COMPS/esp_littlefs" checkout 032f9eb2e291e7c4df63c3e6a3cb3bc766ded495
-#        git -C "$AR_COMPS/esp_littlefs" submodule update --init --recursive
-#else
-#	git -C "$AR_COMPS/esp_littlefs" fetch
-#	git -C "$AR_COMPS/esp_littlefs" pull --ff-only
-#        git -C "$AR_COMPS/esp_littlefs" checkout 032f9eb2e291e7c4df63c3e6a3cb3bc766ded495
-#        git -C "$AR_COMPS/esp_littlefs" submodule update --init --recursive
-#fi
-#if [ $? -ne 0 ]; then exit 1; fi
 
 #
 # CLONE/UPDATE TINYUSB
