531dad7a771a05084f9a5b3e84e4ebefe6f9d3ee
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-22 12:34:13 +0100

    builder updates (#84)
    * Update GH actions
    
    * Update python
    
    * CONFIG_SPI_FLASH_YIELD_DURING_ERASE=y
    
    * Use MacOS ARM
    
    * Update push.yml
    
    * Update push.yml
    
    * add lib Builder branch
    
    * add env
    
    * add comments
    
    * CONFIG_ETH_TRANSMIT_MUTEX
    
    * Fix build of build Platformio manifest (fail in second run)
    
    * fix manifest script build error
    
    * rm outdated code
    
    * don't know why it needs to be restored?!?
    
    * Build Pio framework manifest (#78)
    
    * Update build.sh
    
    * Update gen_pio_frmwk_manifest.py
    
    * Delete core_version.h
    
    * generate core version.h
    
    * Update build.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install -y gperf cmake ninja-build
-pip3 install wheel future pyelftools
+# Ubuntu setup
+# Change in archive-build.sh gawk to awk
+#sudo apt-get install -y gperf cmake ninja-build
+#pip3 install wheel future pyelftools
+
+# MacOS (ARM) setup
+# Change in archive-build.sh awk to gawk
+brew install gsed
+brew install gawk
+brew install gperf
+brew install ninja
+brew install ccache
+python -m pip install --upgrade pip
+pip install wheel future pyelftools
