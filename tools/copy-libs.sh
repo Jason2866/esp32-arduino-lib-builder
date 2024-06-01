45819ac608402a009df134e173396ca0b7c1ef9c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-01 14:21:30 +0200

    more strip

--- tools/copy-libs.sh
+++ tools/copy-libs.sh
@@ -516,6 +516,7 @@ function copy_precompiled_lib(){
 	lib_file="$1"
 	lib_name="$(basename $lib_file)"
 	if [[ $LD_LIBS_SEARCH == *"$lib_name"* ]]; then
+		"$TOOLCHAIN-strip" -g "$lib_file"
 		cp "$lib_file" "$AR_SDK/ld/"
 	fi
 }
@@ -532,7 +533,7 @@ for item; do
 	done
 done
 
-for lib in "openthread" "espressif__esp-tflite-micro" "bt"; do
+for lib in "openthread" "espressif__esp-tflite-micro" "bt" "espressif__esp_modem" "espressif__esp-zboss-lib" "espressif__esp-zigbee-lib" "espressif__mdns" "joltwallet__littlefs"; do
 	if [ -f "$AR_SDK/lib/lib$lib.a" ]; then
 		echo "Stripping $AR_SDK/lib/lib$lib.a"
 		"$TOOLCHAIN-strip" -g "$AR_SDK/lib/lib$lib.a"
@@ -554,6 +555,7 @@ for mem_variant in `jq -c '.mem_variants_files[]' configs/builds.json`; do
 		file=$(echo "$mem_variant" | jq -c '.file' | tr -d '"')
 		out=$(echo "$mem_variant" | jq -c '.out' | tr -d '"')
 		mv "$AR_SDK/$out" "$AR_SDK/$MEMCONF/$file"
+		"$TOOLCHAIN-strip" -g "$file"
 	fi
 done;
 
