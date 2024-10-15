6bd960440f8fcb34f0362e50d979c285b6e103d4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-15 16:45:43 +0200

    Update del_usb_source.py

--- tools/del_usb_source.py
+++ tools/del_usb_source.py
@@ -5,25 +5,27 @@ import shutil
 from os.path import join
 
 
-def main(dir):
-    s2_sdkconfig_path = os.path.join(dir, "esp32s2", "bin", "sdkconfig")
+def main(dir_out):
+    s2_sdkconfig_path = os.path.join(dir_out, "esp32s2", "bin", "sdkconfig")
     if os.path.exists(s2_sdkconfig_path) == False:
       print("*** sdkconfig file for esp32s2 not found, no changes done ***")
       return
+
+    esp32_dir = os.path.join(dir_out, "arduino-esp32", "cores", "esp32")
 
     with open(s2_sdkconfig_path) as src:
         line = src.readline()
         if line.startswith("# CONFIG_TINYUSB_ENABLED is not set"):
             print("*** TinyUSB is disabled, removing not needed USB Source code ***")
-            os.remove("arduino-esp32/cores/esp32/esp32-hal-tinyusb.c")
-            os.remove("arduino-esp32/cores/esp32/esp32-hal-tinyusb.h")
-            os.remove("arduino-esp32/cores/esp32/USB.cpp")
-            os.remove("arduino-esp32/cores/esp32/USB.h")
-            os.remove("arduino-esp32/cores/esp32/USBCDC.cpp")
-            os.remove("arduino-esp32/cores/esp32/USBCDC.h")
-            os.remove("arduino-esp32/cores/esp32/USBMSC.cpp")
-            os.remove("arduino-esp32/cores/esp32/USBMSC.h")
-            shutil.rmtree("arduino-esp32/libraries/USB", ignore_errors=True)
+            os.remove(join(esp32_dir, "esp32-hal-tinyusb.c"))
+            os.remove(join(esp32_dir, "esp32-hal-tinyusb.h"))
+            os.remove(join(esp32_dir, "USB.cpp"))
+            os.remove(join(esp32_dir, "USB.h"))
+            os.remove(join(esp32_dir, "USBCDC.cpp"))
+            os.remove(join(esp32_dir, "USBCDC.h"))
+            os.remove(join(esp32_dir, "USBMSC.cpp"))
+            os.remove(join(esp32_dir, "USBMSC.h"))
+            shutil.rmtree(join(dir_out, "arduino-esp32", "libraries", "USB"), ignore_errors=True)
     return 0
 
 
@@ -32,10 +34,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "-d",
         "--dir",
-        dest="dir",
+        dest="dir_out",
         required=True,
         help="Directory folder where the esp32-s2 'sdkonfig' file is located",
     )
     args = parser.parse_args()
 
-    sys.exit(main(args.dir))
+    sys.exit(main(args.dir_out))
