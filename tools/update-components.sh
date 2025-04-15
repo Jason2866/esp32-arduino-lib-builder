3a38dd35bdb35588d48bfd9d90aedc994e82e3db
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-15 19:00:57 +0200

    Update update-components.sh

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -10,16 +10,12 @@ TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 #
 echo "Updating TinyUSB..."
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-       git clone -b master "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
-       cd "$TINYUSB_REPO_DIR"
-       git checkout 6689a0142dfbb2cf2bb8f7f7e4f4ae8d019648a9
-       cd -
+       git clone -b master --depth 1 "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
 else
        cd $TINYUSB_REPO_DIR
        git pull
        # -ff is for cleaning untracked files as well as submodules
        git clean -ffdx
-       git checkout 6689a0142dfbb2cf2bb8f7f7e4f4ae8d019648a9
        cd -
 fi
 if [ $? -ne 0 ]; then exit 1; fi
