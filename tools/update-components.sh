eb832cfc7375fe1e726e67fd5deb900e962e4b00
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-15 13:24:37 +0200

    Use commit `6689a014...` for tinyusb

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -10,12 +10,15 @@ TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 #
 echo "Updating TinyUSB..."
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-       git clone -b master --depth 1 "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
+       git clone -b master "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
+       cd "$TINYUSB_REPO_DIR"
+       git checkout 6689a0142dfbb2cf2bb8f7f7e4f4ae8d019648a9
+       cd -
 else
        cd $TINYUSB_REPO_DIR
        git pull
        # -ff is for cleaning untracked files as well as submodules
        git clean -ffdx
-       cd -
+       git checkout 6689a0142dfbb2cf2bb8f7f7e4f4ae8d019648a9
 fi
 if [ $? -ne 0 ]; then exit 1; fi
