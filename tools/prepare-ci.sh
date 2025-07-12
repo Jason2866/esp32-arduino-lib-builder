70b2d5b87b02c4220627852164fb91d2504248fc
Jason2866 <obermeier.johann@googlemail.com>
2025-07-12 13:53:49 +0200

    clean up and use 7z

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -7,11 +7,12 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install gperf || true
     brew install ninja || true
     brew install ccache || true
+    brew install 7zip || true
     python3 -m pip install uv
     uv venv
     uv pip install future pyelftools
 else
     echo "Linux detected. Install dependencies..."
-    sudo apt update && sudo apt install -y gperf cmake ninja-build ccache
+    sudo apt update && sudo apt install -y gperf cmake ninja-build ccache p7zip-full
     pip3 install wheel future pyelftools
 fi
