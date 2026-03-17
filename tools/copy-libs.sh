20132ac0a1feabbf2660d443e38ea9305f4c2417
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-17 17:02:17 +0100

    Remove PIO_CC_FLAGS assignment in copy-libs.sh
    Removed PIO_CC_FLAGS assignment while parsing CC file.

--- tools/copy-libs.sh
+++ tools/copy-libs.sh
@@ -107,7 +107,6 @@ for item in "${@:2:${#@}-5}"; do
 		for xitem in `cat "$xfile"`; do
 			echo "Add CC flag '$xitem'"
 			C_FLAGS+="$xitem "
-			PIO_CC_FLAGS+="$xitem "
 			PIO_LD_FLAGS+="$xitem "
 		done
 	elif [ "$prefix" = "-I" ]; then
