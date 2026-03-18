5df95965761ee6f7e54ffe415219339729e8b667
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-18 20:19:17 +0100

    Add file existence checks for parsing flags
    Added checks for file existence before parsing CC, AS, and LD files to prevent errors.

--- tools/copy-libs.sh
+++ tools/copy-libs.sh
@@ -103,10 +103,11 @@ for item in "${@:2:${#@}-5}"; do
 	prefix="${item:0:2}"
 	if [ "${item:0:1}" = "@" ]; then
 		xfile="${item:3:${#item}-5}"
+		if [ ! -f "$xfile" ]; then echo "File '$xfile' does not exist!"; exit 1; fi
 		echo "Parse CC file '$xfile'"
 		for xitem in `cat "$xfile"`; do
-			echo "Add CC flag '$xitem'"
 			C_FLAGS+="$xitem "
+			LD_FLAGS+="$xitem "
 			PIO_LD_FLAGS+="$xitem "
 		done
 	elif [ "$prefix" = "-I" ]; then
@@ -147,21 +148,23 @@ for item in "${@:2:${#@}-5}"; do
 	prefix="${item:0:2}"
 	if [ "${item:0:1}" = "@" ]; then
 		xfile="${item:3:${#item}-5}"
+		if [ ! -f "$xfile" ]; then echo "File '$xfile' does not exist!"; exit 1; fi
 		echo "Parse AS file '$xfile'"
 		for xitem in `cat "$xfile"`; do
+			AS_FLAGS+="$xitem "
 			if [[ "${xitem:0:6}" != "-mtune" && "${xitem:0:6}" != "-specs" ]]; then
-				echo "Add AS flag '$xitem'"
-				AS_FLAGS+="$xitem "
 				PIO_AS_FLAGS+="$xitem "
 			fi
 		done
 	elif [[ "$prefix" != "-I" && "$prefix" != "-D" && "$item" != "-Wall" && "$item" != "-Werror=all"  && "$item" != "-Wextra" && "$item" != "-fno-lto" && "$prefix" != "-O" ]]; then
-		if [[ "${item:0:23}" != "-mfix-esp32-psram-cache" && "${item:0:18}" != "-fmacro-prefix-map" && "${item:0:20}" != "-fdiagnostics-color=" && "${item:0:19}" != "-fdebug-prefix-map=" && "${item:0:6}" != "-mtune" && "${item:0:6}" != "-specs" ]]; then
+		if [[ "${item:0:23}" != "-mfix-esp32-psram-cache" && "${item:0:18}" != "-fmacro-prefix-map" && "${item:0:20}" != "-fdiagnostics-color=" && "${item:0:19}" != "-fdebug-prefix-map=" ]]; then
 			AS_FLAGS+="$item "
-			if [[ $C_FLAGS == *"$item"* ]]; then
-				PIO_CC_FLAGS+="$item "
-			else
-				PIO_AS_FLAGS+="$item "
+			if [[ "${item:0:6}" != "-mtune" && "${item:0:6}" != "-specs" ]]; then
+				if [[ $C_FLAGS == *"$item"* ]]; then
+					PIO_CC_FLAGS+="$item "
+				else
+					PIO_AS_FLAGS+="$item "
+				fi
 			fi
 		fi
 	fi
@@ -176,9 +179,9 @@ for item in "${@:2:${#@}-5}"; do
 	prefix="${item:0:2}"
 	if [ "${item:0:1}" = "@" ]; then
 		xfile="${item:3:${#item}-5}"
+		if [ ! -f "$xfile" ]; then echo "File '$xfile' does not exist!"; exit 1; fi
 		echo "Parse CXX file '$xfile'"
 		for xitem in `cat "$xfile"`; do
-			echo "Add CXX flag '$xitem'"
 			CPP_FLAGS+="$xitem "
 			PIO_CXX_FLAGS+="$xitem "
 		done
@@ -329,9 +332,9 @@ for item; do
 				item="$item"
 			elif [ "${item:0:1}" = "@" ]; then
 				xfile="${item:2:${#item}-3}"
+				if [ ! -f "$xfile" ]; then echo "File '$xfile' does not exist!"; exit 1; fi
 				echo "Parse LD file '$xfile'"
 				for xitem in `cat "$xfile"`; do
-					echo "Add LD flag '$xitem'"
 					LD_FLAGS+="$xitem "
 					PIO_LD_FLAGS+="$xitem "
 				done
