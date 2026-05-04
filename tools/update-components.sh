acee3763e6da0bf5c0c29a18cce44e07a6cbef9c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-05-04 17:27:39 +0200

    Remove TinyUSB update functionality from script
    Removed the TinyUSB update logic from the script.

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -1,21 +1,3 @@
 #/bin/bash
 
 source ./tools/config.sh
-
-TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
-TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
-
-#
-# CLONE/UPDATE TINYUSB
-#
-echo "Updating TinyUSB..."
-if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-       git clone -b master --depth 1 "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
-else
-       cd $TINYUSB_REPO_DIR
-       git pull
-       # -ff is for cleaning untracked files as well as submodules
-       git clean -ffdx
-       cd -
-fi
-if [ $? -ne 0 ]; then exit 1; fi
